adba7bad8f596b20cf05fd90c24aa9439370bd45
chinwoke <canugwara99@gmail.com>
2023-06-11 11:07:14 +0100

    password encoder introduced

--- src/main/java/com/loan/application/system/userManagement/security/SecurityConfiguration.java
+++ src/main/java/com/loan/application/system/userManagement/security/SecurityConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -71,5 +73,9 @@ public class SecurityConfiguration {
             AuthenticationConfiguration authenticationConfiguration
     ) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
